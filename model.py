c865ae1c20ce88d945244b55c74e7005b3c0d895
Denis Candido <denis.candido@venturus.org.br>
2020-09-11 18:38:59 -0300

    Initial

---
+++ model.py
@@ -0,0 +1,18 @@
+import 
+
+
+class ObjectDetector():
+    def __init__(self):
+        pass
+    
+    
+    def serve():
+        pass
+    
+    def infer():
+        pass
+    
+    
+    
+if __name__ == '__main__':
+    pass
